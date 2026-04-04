3708bff22ca8917fb93ff75f43159c13df14bb31
Lucas Lobo <llucaslobo1@gmail.com>
2026-04-04 16:21:06 -0400

    fix(wevhook): Atualizado o default de channel em headers

--- src/middleware/channel.js
+++ src/middleware/channel.js
@@ -1,9 +1,9 @@
 const channelMiddleware = (req, _res, next) => {
-  const headerChannel = req.headers['x-channel']
+  const headerChannel = req.headers.channel || req.headers['x-channel']
   const queryChannel = req.query?.channel
   const bodyChannel = req.body?.channel
 
-  req.channel = bodyChannel || queryChannel || headerChannel || 'whatsapp'
+  req.channel = bodyChannel || queryChannel || headerChannel || 'whaileys'
   next()
 }
 
--- src/utils/fetchWebHook.js
+++ src/utils/fetchWebHook.js
@@ -9,7 +9,8 @@ const fetchWebHook = (wbot) => {
     wbot.webhooks = sessionData.webhooks || []
   }
 
-  wbot.webhooks.push(env.WEBHOOK)
+  if (env.WEBHOOK)wbot.webhooks.push(env.WEBHOOK)
+
   wbot.webhooks = wbot.webhooks.filter(
     (item, index, self) => self.indexOf(item) === index,
   )
